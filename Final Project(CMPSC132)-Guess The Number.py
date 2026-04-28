ae3b7aec975cf8f97e90837a83c2b94a3026cfb3
MarcoL007 <mal6961@psu.edu>
2026-04-28 00:17:59 -0400

    Settings and difficulties

---
+++ Final Project(CMPSC132)-Guess The Number.py
@@ -0,0 +1,18 @@
+import random
+    # Starting by creating the settings first
+def get_game_setttings():
+    settings = {"Easy": {"limit": 10, "attempts": 5},
+                "Medium": {"limit": 50, "attempts": 6},
+                "Hard": {"limit": 100, "attempts": 6}   }
+            #Creating a Dictionary for the levels and attempts
+
+    print("---Select Difficulty---")
+    for lvl in settings:
+        print(f"- {lvl}")
+    difficulty = input("Enter Choice: ").capitalize()
+
+    # Checking for invalid inputs when selecting the level
+    if difficulty not in settings:
+        print("Invalid Input, Defalting to Easy.")
+        return settings["Easy"]
+    return settings[difficulty]
